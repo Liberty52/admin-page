cadc76161a0142a4cf05216bb209f2ffb4b18e52
2hanbyeol1 <2hanbyeol1@naver.com>
2023-05-21 01:18:41 +0900

    필요없는 import 주석 처리

--- src/screen/Customer.js
+++ src/screen/Customer.js
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import { MainContainer } from "./component/main/MainComponent";
 import SideNav from "./component/common/side-nav/SideNav";
 import { Box, Container, Stack, Typography, Input } from "@mui/material";
 import { CustomerTable } from "../component/customer/CustomerTable";
-import { CustomerSearch } from "../component/customer/CustomerSearch";
+// import { CustomerSearch } from "../component/customer/CustomerSearch";
 // import { useCallback, useEffect, useState } from "react";
 
 export default function Customer() {
