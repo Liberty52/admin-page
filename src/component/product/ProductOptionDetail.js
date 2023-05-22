1aa98a0ef8bd1875f137f5fe12a7d061bc88dfb7
kyoohyun <kgh2120@naver.com>
2023-05-22 16:38:21 +0900

    상품 옵션 상태 변경 메세지 수정
    #16

--- src/component/product/ProductOptionDetail.js
+++ src/component/product/ProductOptionDetail.js
@@ -13,23 +13,20 @@ import {Toast} from "../../utils/Toast";
 
 export default function ProductOptionDetail({onEditButtonClicked,detail, actived}){
     const [focused, setFocused] = useState();
-
-
     const onMouseOn = () => {
         setFocused(true)
     }
     const onMouseOut = () => {
         setFocused(false)
     }
-
     const onDeleteButtonClicked = () => {
         Swal.fire({
-            title: '정말로 삭제하시겠습니까?',
-            icon: 'warning',
+            title: '정말로 판매 상태를 변경하시겠습니까?',
+            icon: 'question',
             showCancelButton: true,
             confirmButtonColor: '#d33',
             cancelButtonColor: '#3085d6',
-            confirmButtonText: '삭제하기',
+            confirmButtonText: '변경하기',
             cancelButtonText : '취소하기'
         }).then((result) => {
             if (result.isConfirmed) {
@@ -38,8 +35,8 @@ export default function ProductOptionDetail({onEditButtonClicked,detail, actived
                 }
                 deleteOptionDetail(detail.optionDetailId,data).then(() => {
                     Toast.fire({
-                        icon: 'warning',
-                        title: '삭제가 완료되었습니다'
+                        icon: 'success',
+                        title: '변경이 완료되었습니다'
                     })
                     actived();
                 });
@@ -56,7 +53,6 @@ export default function ProductOptionDetail({onEditButtonClicked,detail, actived
             <ProductOptionDetailWrapper
                 onMouseEnter ={onMouseOn}
                 onMouseLeave ={onMouseOut}
-
             >
             <ProductOptionInput
                                 id={'product-option-input'}
