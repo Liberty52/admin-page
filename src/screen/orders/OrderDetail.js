81f37bfd640240319cc271586210c16f02b390b4
tjswjd7166 <tjswjd7166@gmail.com>
2023-06-03 20:55:32 +0900

    이미지다운로드수정

--- src/screen/orders/OrderDetail.js
+++ src/screen/orders/OrderDetail.js
@@ -1,42 +1,41 @@
 import { MainContainer } from "../component/main/MainComponent";
 import SideNav from "../component/common/side-nav/SideNav";
-import "./OrderDetail.css";
-import { useParams } from "react-router-dom";
-import React, { useState, useEffect, useRef } from "react";
-import { Button } from "@mui/material";
-import CancelModal from "../../component/order/CancelModal";
-import {
-  fetchOrderDetail,
-  getCanceledOrderDetails,
-  approveCancel,
-} from "../../axios/Orders";
+import './OrderDetail.css';
+import { useParams } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { fetchOrderDetail } from '../../axios/Orders';
+import Button from '../../component/Button';
+
 
 ////////////////////////////
-function Border() {
-  return <div className="OrderBorder"></div>;
+function Border(){
+  return <div className="OrderBorder"></div>
 }
 
 ///////////////////////////
 
+
 function OrderTitle() {
-  return (
-    <div className="OrderTitle-h1">
-      <h1>주문상세</h1>
-      <Border />
-    </div>
-  );
+    return (
+        <div className="OrderTitle-h1">
+        <h1>주문상세</h1>
+        <Border/>
+        </div>
+    );
 }
 
+
 function OrderImage({ order }) {
+
   const handleImageDownload = async () => {
     for (const product of order.products) {
       try {
-        const response = await fetch(product.productUrl);
+        const response = await fetch(product.productUrl, {cache:'no-cache'});
         const blob = await response.blob();
         const url = window.URL.createObjectURL(blob);
-        const link = document.createElement("a");
+        const link = document.createElement('a');
         link.href = url;
-        link.setAttribute("download", product.name + ".png");
+        link.setAttribute('download', product.name + '.png');
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -54,35 +53,33 @@ function OrderImage({ order }) {
 }
 
 function OrderInquiry() {
-  return (
-    <div className="Inquiry-Detail-wrapper">
-      <div>상품명</div>
-      <div>상품금액</div>
-      <div>배송비</div>
-      <div>수량</div>
-      <div>상품합계금액</div>
-    </div>
-  );
-}
-function OrderInquiryDetail({ order }) {
-  return (
-    <>
-      {order.products.map((product, index) => (
-        <div key={index} className="Inquiry-Delivery-wrapper">
-          <div>
-            <img src={product.productUrl} alt={product.name} />
+    return (
+      <div className="Inquiry-Detail-wrapper">
+        <div>상품명</div>
+        <div>상품금액</div>
+        <div>배송비</div>
+        <div>수량</div>
+        <div>상품합계금액</div>
+      </div>
+    );
+  }
+  function OrderInquiryDetail({ order }){
+    return (
+      <>
+        {order.products.map((product, index) => (
+          <div key={index} className="Inquiry-Delivery-wrapper">
+            <div><img src={product.productUrl} alt={product.name} /></div>
+            <div>{product.name}</div>
+            <div>{product.price}원</div>
+            <div>{order.deliveryFee}원</div>
+            <div>{product.quantity}</div>
+            <div>{product.price * product.quantity}원</div>
           </div>
-          <div>{product.name}</div>
-          <div>{product.price}원</div>
-          <div>{order.deliveryFee}원</div>
-          <div>{product.quantity}</div>
-          <div>{product.price * product.quantity}원</div>
-        </div>
-      ))}
-    </>
-  );
-}
-function OrderInquiryDelivery({ order }) {
+        ))}
+      </>
+    );
+  }
+function OrderInquiryDelivery({ order }){
   return (
     <div className="Inquiry-Delivery-small-wrapper">
       <div>상품합계금액 : {order.totalProductPrice}원</div>
@@ -92,9 +89,10 @@ function OrderInquiryDelivery({ order }) {
   );
 }
 
-function OrderInquiryDelivery2({ order }) {
+function OrderInquiryDelivery2({ order }){
   return (
     <table className="Inquiry-Delivery">
+
       <tr className="Inquiry-Delivery-top">
         <th className="cell-top">상품합계금액</th>
         <th className="cell-top">배송비</th>
@@ -103,19 +101,19 @@ function OrderInquiryDelivery2({ order }) {
       </tr>
       <tr className="Inquiry-Delivery-bottom">
         <td className="cell-bottom">{order.totalProductPrice}원</td>
-        <td className="cell-bottom">{order.deliveryFee}원</td>
+        <td className="cell-bottom" >{order.deliveryFee}원</td>
         <td className="cell-bottom">0원</td>
         <td className="cell-bottom">{order.totalPrice}원</td>
-      </tr>
-    </table>
+    </tr>
+  </table>
   );
 }
 
-function OrderPerson({ order }) {
+function OrderPerson({order}) {
   return (
-    <>
+      <>
       <h2>주문자 정보</h2>
-      <Border />
+      <Border/>
       <div className="grid">
         <div className="Order-common">주문자명</div>
         <div className="Order-common">{order.customerName}</div>
@@ -125,18 +123,18 @@ function OrderPerson({ order }) {
         <div className="Order-common">{order.receiverPhoneNumber}</div>
       </div>
       <div className="grid">
-        <div className="Order-common">메일</div>
-        <div className="Order-common">{order.receiverEmail}</div>
-      </div>
-    </>
+      <div className="Order-common">메일</div>
+      <div className="Order-common">{order.receiverEmail}</div>
+    </div>
+      </>
   );
 }
 
-function OrderDelivery({ order }) {
+function OrderDelivery({order}) {
   return (
-    <>
+      <>
       <h2>배송지 정보</h2>
-      <Border />
+      <Border/>
       <div className="grid">
         <div className="Order-common">주문자명</div>
         <div className="Order-common">{order.customerName}</div>
@@ -149,18 +147,16 @@ function OrderDelivery({ order }) {
         <div className="Order-common">휴대폰번호</div>
         <div className="Order-common">{order.receiverPhoneNumber}</div>
       </div>
-    </>
+      </>
   );
 }
-function OrderPayment({ order }) {
+function OrderPayment({order}) {
   let paymentDetail;
 
   switch (order.paymentType) {
-    case "신용카드":
+    case '신용카드':
       paymentDetail = (
-        <>
-          {" "}
-          <div className="grid">
+        <> <div className="grid">
             <div className="Order-common">카드사 이름</div>
             <div className="Order-common">{order.paymentInfo.cardName}</div>
           </div>
@@ -179,7 +175,7 @@ function OrderPayment({ order }) {
         </>
       );
       break;
-    case "가상 계좌":
+    case '가상 계좌':
       paymentDetail = (
         <>
           <div className="grid">
@@ -188,30 +184,20 @@ function OrderPayment({ order }) {
           </div>
           <div className="grid">
             <div className="Order-common">입금자 은행</div>
-            <div className="Order-common">
-              {order.paymentInfo.depositorBank}
-            </div>
+            <div className="Order-common">{order.paymentInfo.depositorBank}</div>
           </div>
           <div className="grid">
             <div className="Order-common">입금자 이름</div>
-            <div className="Order-common">
-              {order.paymentInfo.depositorName}
-            </div>
+            <div className="Order-common">{order.paymentInfo.depositorName}</div>
           </div>
 
           <div className="grid">
             <div className="Order-common">입금자 계좌번호</div>
-            <div className="Order-common">
-              {order.paymentInfo.depositorAccount}
-            </div>
+            <div className="Order-common">{order.paymentInfo.depositorAccount}</div>
           </div>
           <div className="grid">
             <div className="Order-common">현금영수증 신청 여부</div>
-            <div className="Order-common">
-              {order.paymentInfo.isApplyCashReceipt
-                ? "신청됨"
-                : "신청되지 않음"}
-            </div>
+            <div className="Order-common">{order.paymentInfo.isApplyCashReceipt ? "신청됨" : "신청되지 않음"}</div>
           </div>
           <div className="grid">
             <div className="Order-common">결제 일자</div>
@@ -226,9 +212,9 @@ function OrderPayment({ order }) {
       );
   }
   return (
-    <>
+      <>
       <h2>결제 정보</h2>
-      <Border />
+      <Border/>
       <div className="grid">
         <div className="Order-common">상품합계금액</div>
         <div className="Order-common">{order.totalProductPrice}원</div>
@@ -246,119 +232,40 @@ function OrderPayment({ order }) {
         <div className="Order-common">{order.paymentType}</div>
       </div>
       {paymentDetail}
-    </>
+      </>
   );
 }
-
-function CancelInfo({ orderId, cancelInfo }) {
-  const [modal, showModal] = useState(false);
-  return (
-    <>
-      {modal && (
-        <CancelModal orderId={orderId} closeModal={() => showModal(false)} />
-      )}
-      <h2>취소 정보</h2>
-      <Border />
-      <div className="grid">
-        <div className="Order-common">취소사유</div>
-        <div className="Order-common">{cancelInfo.reason}</div>
-      </div>
-      <div className="grid">
-        <div className="Order-common">요청일시</div>
-        <div className="Order-common">{cancelInfo.reqAt}</div>
-      </div>
-      <div className="grid">
-        <div className="Order-common">취소일시</div>
-        <div className="Order-common">{cancelInfo.canceledAt}</div>
-      </div>
-      {cancelInfo.approvedAdminName ? (
-        <>
-          <div className="grid">
-            <div className="Order-common">취소요금</div>
-            <div className="Order-common">
-              &#8361;
-              {cancelInfo.fee.toLocaleString("ko-KR")}
-            </div>
-          </div>
-          <div className="grid">
-            <div className="Order-common">담당자명</div>
-            <div className="Order-common">{cancelInfo.approvedAdminName}</div>
-          </div>
-        </>
-      ) : (
-        <Button
-          variant="outlined"
-          sx={{
-            width: "100%",
-            fontWeight: "bold",
-            fontSize: 15,
-            borderColor: "black",
-            color: "black",
-            "&:hover": {
-              borderColor: "black",
-              backgroundColor: "white",
-            },
-          }}
-          onClick={() => showModal(true)}
-        >
-          주문 취소 승인
-        </Button>
-      )}
-    </>
-  );
-}
-function ReOrderDetail() {
+function ReOrderDetail(){
   const goBack = () => {
-    window.location.href = "/order";
-  };
+    window.location.href = '/order';
+  }
 
   return (
     <div className="ReOrder">
-      <Button
-        variant="contained"
-        sx={{
-          backgroundColor: "black",
-          width: "100%",
-          fontSize: 15,
-          "&:hover": {
-            backgroundColor: "black",
-          },
-        }}
-        onClick={goBack}
-      >
-        되돌아가기
-      </Button>
+      <Button onClick={goBack} text ="되돌아가기"></Button>
     </div>
   );
 }
 
-export default function OrderDetail({ canceled }) {
+export default function OrderDetail() {
   const { orderId } = useParams();
   const [order, setOrder] = useState(null);
-  const [cancelInfo, setCancelInfo] = useState(null);
 
   useEffect(() => {
-    if (canceled) {
-      getCanceledOrderDetails(orderId)
-        .then((res) => {
-          setOrder(res.data.basicOrderDetail);
-          setCancelInfo(res.data.canceledInfo);
-        })
-        .catch((err) => alert(err.response.data.error_message));
-    } else {
-      const fetchData = async () => {
-        const data = await fetchOrderDetail(orderId);
-        if (data) {
-          setOrder(data);
-        }
-      };
-      fetchData();
-    }
+    const fetchData = async () => {
+      const data = await fetchOrderDetail(orderId);
+      if (data) {
+        setOrder(data);
+      }
+    };
+
+    fetchData();
   }, [orderId]);
 
   if (!order) {
     return <p>Loading...</p>;
   }
+
 
   return (
     <div className="MainContainer">
@@ -368,19 +275,19 @@ export default function OrderDetail({ canceled }) {
       </div>
       <div className="Right-Container">
         <div className="OrderDetail">
-          <OrderTitle />
-          <OrderInquiry />
-          <OrderInquiryDetail order={order} />
-          <OrderImage order={order} />
-          <OrderInquiryDelivery order={order} />
-          <OrderInquiryDelivery2 order={order} />
-          <OrderPerson order={order} />
-          <OrderDelivery order={order} />
-          <OrderPayment order={order} />
-          {canceled && <CancelInfo orderId={orderId} cancelInfo={cancelInfo} />}
-          <ReOrderDetail />
+            <OrderTitle/>
+            <OrderInquiry/>
+            <OrderInquiryDetail order={order}/>
+            <OrderImage order={order}/>
+            <OrderInquiryDelivery order={order}/>
+            <OrderInquiryDelivery2 order={order}/>
+            <OrderPerson order={order}/>
+            <OrderDelivery order={order}/>
+            <OrderPayment order={order}/>
+            <ReOrderDetail/>
         </div>
       </div>
     </div>
   );
 }
+
