5574b71e3da490fd571610de27bc633965621365
2hanbyeol1 <2hanbyeol1@naver.com>
2023-06-05 20:16:55 +0900

    로그인 성공 alert 삭제

--- src/axios/login.js
+++ src/axios/login.js
@@ -1,46 +1,43 @@
 import request from "./axios";
-import {ACCESS_TOKEN, REFRESH_TOKEN} from "../constants/token";
-import {LOGIN, TOKEN_REFRESH} from "../constants/api";
-import axios from "axios";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants/token";
+import { LOGIN, TOKEN_REFRESH } from "../constants/api";
 
 export function post(dto, checked) {
-    request
-        .post(LOGIN(), JSON.stringify(dto), {
-            headers: {
-                'Content-Type': `application/json`,
-            },
-        })
-        .then(response => {
-            alert(response.data.name + '님 환영합니다!');
-            if (checked) {
-                localStorage.setItem(REFRESH_TOKEN, response.headers.refresh);
-            }
-            sessionStorage.setItem(ACCESS_TOKEN, response.headers.access);
-            window.location.replace('/');
-        })
-        .catch(e => {
-            if (e.response) {
-                if (e.response.status === 401) alert('로그인 실패.');
-            }
-        });
+  request
+    .post(LOGIN(), JSON.stringify(dto), {
+      headers: {
+        "Content-Type": `application/json`,
+      },
+    })
+    .then((response) => {
+      if (checked) {
+        localStorage.setItem(REFRESH_TOKEN, response.headers.refresh);
+      }
+      sessionStorage.setItem(ACCESS_TOKEN, response.headers.access);
+      window.location.replace("/");
+    })
+    .catch(() => {
+      alert("로그인 실패.");
+    });
 }
 
-export function refresh(){
-
-    request.get(TOKEN_REFRESH(), // token refresh api
-        {
-            headers : {
-                "LB-RefreshToken" : localStorage.getItem(REFRESH_TOKEN)
-            },
-        }
-    ).then(res => {
-        sessionStorage.setItem(ACCESS_TOKEN, res.headers.access);
-        localStorage.setItem(REFRESH_TOKEN, res.headers.refresh)
-    }).catch(() => {
-        sessionStorage.removeItem(ACCESS_TOKEN);
-        localStorage.removeItem(REFRESH_TOKEN)
-        window.location.href = '/login';
+export function refresh() {
+  request
+    .get(
+      TOKEN_REFRESH(), // token refresh api
+      {
+        headers: {
+          "LB-RefreshToken": localStorage.getItem(REFRESH_TOKEN),
+        },
+      }
+    )
+    .then((res) => {
+      sessionStorage.setItem(ACCESS_TOKEN, res.headers.access);
+      localStorage.setItem(REFRESH_TOKEN, res.headers.refresh);
     })
-
-
+    .catch(() => {
+      sessionStorage.removeItem(ACCESS_TOKEN);
+      localStorage.removeItem(REFRESH_TOKEN);
+      window.location.href = "/login";
+    });
 }
