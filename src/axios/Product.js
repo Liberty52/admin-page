c13805f5c392a654bcdd42c3fd4dcea8c1ae93c6
2hanbyeol1 <2hanbyeol1@naver.com>
2023-09-23 17:44:39 +0900

    [FEAT] 소개 이미지 업로드

--- src/axios/Product.js
+++ src/axios/Product.js
@@ -1,99 +1,103 @@
 import request from "./axios";
 import {
-    ADD_PRODUCT_OPTION,
-    ADD_PRODUCT_OPTION_DETAIL,
-    CHANGE_PRODUCT_ON_SALE,
-    CHANGE_PRODUCT_OPTION_DETAIL_ON_SALE,
-    PRODUCT_DETAIL,
-    PRODUCT_LIST,
-    PRODUCT_OPTION_LIST,
-    UPDATE_PRODUCT_OPTION,
-    UPDATE_PRODUCT_OPTION_DETAIL
+  ADD_PRODUCT_OPTION,
+  ADD_PRODUCT_OPTION_DETAIL,
+  ADD_PRODUCT_INTRODUCTION,
+  CHANGE_PRODUCT_ON_SALE,
+  CHANGE_PRODUCT_OPTION_DETAIL_ON_SALE,
+  PRODUCT_DETAIL,
+  PRODUCT_LIST,
+  PRODUCT_OPTION_LIST,
+  UPDATE_PRODUCT_OPTION,
+  UPDATE_PRODUCT_OPTION_DETAIL,
 } from "../constants/api";
-import {ACCESS_TOKEN} from "../constants/token";
+import { ACCESS_TOKEN } from "../constants/token";
 
-export  const retrieveProduct = () =>{
-    return request.get(PRODUCT_LIST(),{
-        headers : {
-            Authorization : sessionStorage.getItem(ACCESS_TOKEN)
-        }
-    })
-}
+export const retrieveProduct = () => {
+  return request.get(PRODUCT_LIST(), {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+    },
+  });
+};
 
-export  const retrieveProductDetail = (productId) =>{
-    return request.get(PRODUCT_DETAIL(productId),{
-        headers : {
-            Authorization : sessionStorage.getItem(ACCESS_TOKEN)
-        }
-    })
-}
+export const retrieveProductDetail = (productId) => {
+  return request.get(PRODUCT_DETAIL(productId), {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+    },
+  });
+};
 
-export const retrieveProductOptionList = (productId, onSale)=>{
-    return request.get(PRODUCT_OPTION_LIST(productId,onSale),{
-        headers : {
-            Authorization : sessionStorage.getItem(ACCESS_TOKEN)
-        }
-    })
-}
+export const retrieveProductOptionList = (productId, onSale) => {
+  return request.get(PRODUCT_OPTION_LIST(productId, onSale), {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+    },
+  });
+};
 
 export const addOptionDetail = (optionId, data) => {
-    return request.post(ADD_PRODUCT_OPTION_DETAIL(optionId),
-        data
-        ,{
-        headers : {
-            Authorization : sessionStorage.getItem(ACCESS_TOKEN)
-        }
-    })
-}
+  return request.post(ADD_PRODUCT_OPTION_DETAIL(optionId), data, {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+    },
+  });
+};
 
 export const updateOptionDetail = (optionDetailId, data) => {
-    return request.put(UPDATE_PRODUCT_OPTION_DETAIL(optionDetailId),
-        data
-        ,{
-            headers : {
-                Authorization : sessionStorage.getItem(ACCESS_TOKEN)
-            }
-        })
-}
+  return request.put(UPDATE_PRODUCT_OPTION_DETAIL(optionDetailId), data, {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+    },
+  });
+};
 
 export const changeOptionDetailOnSale = (optionDetailId) => {
-    return request({
-        url : CHANGE_PRODUCT_OPTION_DETAIL_ON_SALE(optionDetailId),
-        method : "PUT",
-        headers : {
-                Authorization : sessionStorage.getItem(ACCESS_TOKEN),
-                "Content-Type" : "application/json"
-        }
-    })
-}
+  return request({
+    url: CHANGE_PRODUCT_OPTION_DETAIL_ON_SALE(optionDetailId),
+    method: "PUT",
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export const addProductOption = (productId, data) => {
-    return request.post(ADD_PRODUCT_OPTION(productId),
-        data
-        ,{
-            headers : {
-                Authorization : sessionStorage.getItem(ACCESS_TOKEN)
-            }
-        })
-}
+  return request.post(ADD_PRODUCT_OPTION(productId), data, {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+    },
+  });
+};
 
 export const updateProductOption = (optionId, data) => {
-    return request.put(UPDATE_PRODUCT_OPTION(optionId),
-        data
-        ,{
-            headers : {
-                Authorization : sessionStorage.getItem(ACCESS_TOKEN)
-            }
-        })
-}
+  return request.put(UPDATE_PRODUCT_OPTION(optionId), data, {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+    },
+  });
+};
 
 export const changeProductOptionOnSale = (optionId) => {
-    return request({
-        url : CHANGE_PRODUCT_ON_SALE(optionId),
-        method : "PUT",
-        headers : {
-            Authorization : sessionStorage.getItem(ACCESS_TOKEN),
-            "Content-Type" : "application/json"
-        }
-    })
-}
+  return request({
+    url: CHANGE_PRODUCT_ON_SALE(optionId),
+    method: "PUT",
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export const addProductIntroduction = (productId, imageFile) => {
+  const formData = new FormData();
+  formData.append("imageFile", imageFile);
+  return request.post(ADD_PRODUCT_INTRODUCTION(productId), formData, {
+    headers: {
+      Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
--- src/component/product/ProductIntroPanel.js
+++ src/component/product/ProductIntroPanel.js
@@ -1,18 +1,40 @@
 import ImageInput from "./ProductIntroImageInput";
 import { Button } from "@mui/material";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import example from "../../image/preview-example.png";
+import { addProductIntroduction } from "../../axios/Product";
 
 export default function ProductIntroPanel() {
+  const { productId } = useParams();
   const [imgFile, setImgFile] = useState(undefined);
 
   useEffect(() => {
-    // TODO - 현재 소개 이미지 가져와져 set
+    // [TODO] 상품 소개 관리 조회 (관리자)
     setImgFile(example);
   }, []);
 
+  async function upload(productId, imgFile) {
+    const response = await addProductIntroduction(productId, imgFile);
+    if (response.status === 201) {
+      alert("소개 이미지 변경 성공!");
+    } else {
+      alert(`[${response.status} ERROR] 소개 이미지 변경 실패.`);
+    }
+  }
+
   return (
-    <>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        const file = event.target.file.files[0];
+        if (file) {
+          upload(productId, file);
+        } else {
+          alert("소개 이미지를 수정한 후 업로드가 가능합니다.");
+        }
+      }}
+    >
       <div
         style={{
           display: "flex",
@@ -21,35 +43,32 @@ export default function ProductIntroPanel() {
         }}
       >
         <Button
+          type="button"
           sx={{ marginRight: 1, color: "black", borderColor: "black" }}
           variant="outlined"
           disabled={imgFile === undefined}
           onClick={() => {
-            window.alert(
-              "구현되지 않은 기능입니다. liberty 52 order 화면을 보여줍니다."
-            );
-            window.open(
-              "https://liberty52.com/order",
-              "_blank",
-              "noopener, noreferrer"
-            );
+            // [TODO] 상품 소개 미리보기
+            window.alert("구현되지 않은 기능입니다.");
+            // window.open(
+            //   "https://liberty52.com/order",
+            //   "_blank",
+            //   "noopener, noreferrer"
+            // );
           }}
         >
           미리보기
         </Button>
         <Button
+          type="submit"
           sx={{ fontWeight: "bold" }}
           variant="outlined"
           disabled={imgFile === undefined}
-          onClick={() => {
-            window.alert("구현되지 않은 기능입니다. axios post");
-            // 업로드
-          }}
         >
           업로드
         </Button>
       </div>
       <ImageInput imgFile={imgFile} setImgFile={setImgFile} />
-    </>
+    </form>
   );
 }
