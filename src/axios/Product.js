c6b125ec8eba5fd88cc2c67047015d48b5fea369
2hanbyeol1 <2hanbyeol1@naver.com>
2023-09-27 18:51:19 +0900

    [hotfix] 상품 소개 관리 수정 오류 해결

--- src/axios/Product.js
+++ src/axios/Product.js
@@ -104,7 +104,7 @@ export const addProductIntroduction = (productId, imageFile) => {
 
 export const modifyProductIntroduction = (productId, imageFile) => {
   const formData = new FormData();
-  formData.append("imageFile", imageFile);
+  formData.append("images", imageFile);
   return request.patch(ADD_PRODUCT_INTRODUCTION(productId), formData, {
     headers: {
       Authorization: sessionStorage.getItem(ACCESS_TOKEN),
