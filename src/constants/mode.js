c5e217f64291b42963963a2153ae80424d84bcde
Geonwoo Kim <96376539+DryRains@users.noreply.github.com>
2023-10-17 21:05:31 +0900

    [LIB-99] Question Dialog - 수정 모드 삭제 & Review Dialog - 기존에 존재하던 버그 수정 (#100)
    * Question Dialog - 수정 모드 삭제 & Review Dialog - 기존에 존재하던 버그 수정
    
    * constants 활용

--- src/constants/mode.js
+++ src/constants/mode.js
@@ -1,9 +1,10 @@
 export const ModalMode = {
-    ADD : "ADD",
-    EDIT : "EDIT"
-}
+  ADD: "ADD",
+  EDIT: "EDIT",
+  VIEW: "VIEW",
+};
 
 export const LicenseModalMode = {
     MODIFY: "MODIFY",
     ENROLL: "ENROLL"
-}
+};
