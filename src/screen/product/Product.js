2a1b4a95f458bfc7658c0b1f652e812062f8acf9
2hanbyeol1 <2hanbyeol1@naver.com>
2023-11-05 22:10:56 +0900

    [REFACTOR] useless fragment 삭제

--- src/screen/product/Product.js
+++ src/screen/product/Product.js
@@ -40,59 +40,57 @@ export default function Product() {
   };
 
   return (
-    <>
-      <MainContainer>
-        <SideNav />
-        <Box
-          component='main'
-          sx={{
-            padding: '0 5%',
-            flexGrow: 1,
-            py: 8,
-          }}
-        >
-          <Container sx={{ marginLeft: '30px' }} maxWidth='xl'>
-            <ProductHeaderWrapper
-              direction={'row'}
-              justifyContent={'space-between'}
-              alignItems={'center'}
-            >
-              <ProductTitle>상품 관리</ProductTitle>
-              <ProductAddButtonWrapper>
-                <ControlPointIcon
-                  sx={{ fontWeight: 'bold' }}
-                  variant='outlined'
-                  onClick={openLicenseOpen}
-                />
-              </ProductAddButtonWrapper>
-            </ProductHeaderWrapper>
+    <MainContainer>
+      <SideNav />
+      <Box
+        component='main'
+        sx={{
+          padding: '0 5%',
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container sx={{ marginLeft: '30px' }} maxWidth='xl'>
+          <ProductHeaderWrapper
+            direction={'row'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+          >
+            <ProductTitle>상품 관리</ProductTitle>
+            <ProductAddButtonWrapper>
+              <ControlPointIcon
+                sx={{ fontWeight: 'bold' }}
+                variant='outlined'
+                onClick={openLicenseOpen}
+              />
+            </ProductAddButtonWrapper>
+          </ProductHeaderWrapper>
 
-            <ProductBox useFlexGap flexWrap={'wrap'} direction={'row'} spacing={2}>
-              {product !== undefined ? (
-                product?.map((p) => (
-                  <ProductItem
-                    key={p.id}
-                    id={p.id}
-                    name={p.name}
-                    price={p.price}
-                    stock={p.stock}
-                    rating={p.meanRating}
-                    nOfRating={p.ratingCount}
-                    img={p.pictureUrl}
-                    state={p.state}
-                    custom={p.custom}
-                  />
-                ))
-              ) : (
-                <></>
-              )}
-            </ProductBox>
-            <Stack>
-              <LicenseOption open={open} onClose={closeLicenseOption} getProduct={getProduct} />
-            </Stack>
-          </Container>
-        </Box>
-      </MainContainer>
-    </>
+          <ProductBox useFlexGap flexWrap={'wrap'} direction={'row'} spacing={2}>
+            {product !== undefined ? (
+              product?.map((p) => (
+                <ProductItem
+                  key={p.id}
+                  id={p.id}
+                  name={p.name}
+                  price={p.price}
+                  stock={p.stock}
+                  rating={p.meanRating}
+                  nOfRating={p.ratingCount}
+                  img={p.pictureUrl}
+                  state={p.state}
+                  custom={p.custom}
+                />
+              ))
+            ) : (
+              <></>
+            )}
+          </ProductBox>
+          <Stack>
+            <LicenseOption open={open} onClose={closeLicenseOption} getProduct={getProduct} />
+          </Stack>
+        </Container>
+      </Box>
+    </MainContainer>
   );
 }
