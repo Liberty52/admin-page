e5ce103376b2614177c275a3f213755a42818cc4
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2023-11-06 23:49:12 +0900

    [ISSUE-279] 상품 소개 CRUD BUGFIX (#136)
    * [hotfix] 상품 소개 관리 수정 오류 해결
    
    * [BUGFIX] 상품 소개 CREATE
    
    * [REFACTOR] useless fragment 삭제
    
    * [REFACTOR] 함수 정리
    
    * [BUGFIX] 상품 소개 DELETE
    
    * [REFACTOR] notice editor react-editor로 변경

--- src/screen/product/Product.js
+++ src/screen/product/Product.js
@@ -17,7 +17,12 @@ import { Stack } from '@mui/material';
 export default function Product() {
   const [product, setProduct] = useState([]);
   const [open, setOpen] = useState(false);
-  const effect = async () => {
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  const getProduct = async () => {
     try {
       const response = await retrieveProduct();
       setProduct(response.data);
@@ -25,13 +30,7 @@ export default function Product() {
       console.error(e);
     }
   };
-  useEffect(() => {
-    effect();
-  }, []);
 
-  const getProduct = () => {
-    effect();
-  };
   const openLicenseOpen = () => {
     setOpen(true);
   };
@@ -40,59 +39,57 @@ export default function Product() {
   };
 
   return (
-    <>
-      <MainContainer>
-        <SideNav />
-        <Box
-          component='main'
-          sx={{
-            padding: '0 5%',
-            flexGrow: 1,
-            py: 8,
-          }}
-        >
-          <Container sx={{ marginLeft: '30px' }} maxWidth='xl'>
-            <ProductHeaderWrapper
-              direction={'row'}
-              justifyContent={'space-between'}
-              alignItems={'center'}
-            >
-              <ProductTitle>상품 관리</ProductTitle>
-              <ProductAddButtonWrapper>
-                <ControlPointIcon
-                  sx={{ fontWeight: 'bold' }}
-                  variant='outlined'
-                  onClick={openLicenseOpen}
-                />
-              </ProductAddButtonWrapper>
-            </ProductHeaderWrapper>
+    <MainContainer>
+      <SideNav />
+      <Box
+        component='main'
+        sx={{
+          padding: '0 5%',
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container sx={{ marginLeft: '30px' }} maxWidth='xl'>
+          <ProductHeaderWrapper
+            direction={'row'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+          >
+            <ProductTitle>상품 관리</ProductTitle>
+            <ProductAddButtonWrapper>
+              <ControlPointIcon
+                sx={{ fontWeight: 'bold' }}
+                variant='outlined'
+                onClick={openLicenseOpen}
+              />
+            </ProductAddButtonWrapper>
+          </ProductHeaderWrapper>
 
-            <ProductBox useFlexGap flexWrap={'wrap'} direction={'row'} spacing={2}>
-              {product !== undefined ? (
-                product?.map((p) => (
-                  <ProductItem
-                    key={p.id}
-                    id={p.id}
-                    name={p.name}
-                    price={p.price}
-                    stock={p.stock}
-                    rating={p.meanRating}
-                    nOfRating={p.ratingCount}
-                    img={p.pictureUrl}
-                    state={p.state}
-                    custom={p.custom}
-                  />
-                ))
-              ) : (
-                <></>
-              )}
-            </ProductBox>
-            <Stack>
-              <LicenseOption open={open} onClose={closeLicenseOption} getProduct={getProduct} />
-            </Stack>
-          </Container>
-        </Box>
-      </MainContainer>
-    </>
+          <ProductBox useFlexGap flexWrap={'wrap'} direction={'row'} spacing={2}>
+            {product !== undefined ? (
+              product?.map((p) => (
+                <ProductItem
+                  key={p.id}
+                  id={p.id}
+                  name={p.name}
+                  price={p.price}
+                  stock={p.stock}
+                  rating={p.meanRating}
+                  nOfRating={p.ratingCount}
+                  img={p.pictureUrl}
+                  state={p.state}
+                  custom={p.custom}
+                />
+              ))
+            ) : (
+              <></>
+            )}
+          </ProductBox>
+          <Stack>
+            <LicenseOption open={open} onClose={closeLicenseOption} getProduct={getProduct} />
+          </Stack>
+        </Container>
+      </Box>
+    </MainContainer>
   );
 }
